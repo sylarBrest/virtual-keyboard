61f4731602697a2a321dd1f5f41575f6930979b8
sylarBrest <aostrovsky87@gmail.com>
2022-05-05 12:10:21 +0300

    refactor: refactor code to airbnb-rules

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -9,6 +9,7 @@ export default class Keyboard {
     this.properties = {
       value: '',
       isCapsLock: false,
+      isShiftPressed: false,
       lang: 'en',
     };
   }
@@ -26,7 +27,8 @@ export default class Keyboard {
     this.elements.keyboardContainer = document.createElement('div');
     this.elements.keyboardContainer.classList.add('keyboard');
     this.elements.keys = this.elements.keyboardContainer.querySelectorAll('.key');
-    this.elements.keys.forEach((key) => key.addEventListener('click', () => console.log('qqq')));
+
+    this.render();
   }
 
   createKeys() {
@@ -46,8 +48,7 @@ export default class Keyboard {
       let content;
       keyElement.setAttribute('type', 'button');
       keyElement.classList.add('key');
-      keyElement.textContent = key.toLowerCase();
-      keyElement.innerHTML = '';
+
       switch (key) {
         // Buttons with words on it
         case 'del':
@@ -66,11 +67,11 @@ export default class Keyboard {
           break;
         // Normal buttons with icons
         case 'backtick':
-          keyElement.textContent = '`';
+          keyElement.classList.add('symbol');
+          [keyElement.textContent, content] = ['`', '`'];
           break;
         case 'tab':
-          keyElement.innerHTML = iconForKey('keyboard_tab');
-          content = '\t';
+          [keyElement.innerHTML, content] = [iconForKey('keyboard_tab'), '\t'];
           break;
         case 'win':
           keyElement.innerHTML = iconForKey('window');
@@ -95,29 +96,29 @@ export default class Keyboard {
         case 'capslock':
           keyElement.classList.add('wide');
           keyElement.innerHTML = iconForKey('keyboard_capslock');
-          keyElement.addEventListener('click', () => this.capsLockOn());
+          keyElement.addEventListener('click', () => {
+            keyElement.classList.toggle('pressed');
+            this.capsLockOn();
+          });
           break;
         case 'enter':
           keyElement.classList.add('wide');
-          keyElement.innerHTML = iconForKey('keyboard_return');
-          content = '\n';
+          [keyElement.innerHTML, content] = [iconForKey('keyboard_return'), '\n'];
           break;
         case 'leftshift':
         case 'rightshift':
           keyElement.classList.add('wide');
           keyElement.innerHTML = iconForKey('publish');
+          keyElement.addEventListener('click', () => this.shiftKeyOn());
           break;
           // Ultrawide (spacebar)
         case 'spacebar':
           keyElement.classList.add('ultrawide');
-          keyElement.innerHTML = iconForKey('space_bar');
-          content = ' ';
+          [keyElement.innerHTML, content] = [iconForKey('space_bar'), ' '];
           break;
         default:
           keyElement.classList.add('symbol');
-          keyElement.innerHTML = key;
-          content = keyElement.innerHTML;
-          this.properties.value += (this.properties.isCapsLock) ? key.toUpperCase() : key.toLowerCase();
+          [content, keyElement.innerHTML] = [key, key];
           break;
       }
       keyElement.addEventListener('click', () => this.mouseClick(content));
@@ -127,19 +128,27 @@ export default class Keyboard {
     return keyboardFragment;
   }
 
-  mouseClick(content) {
-    const con = (this.properties.isCapsLock) ? content.toUpperCase() : content;
-    this.elements.textarea.textContent += con || '';
+  mouseClick(content = '') {
+    const value = (this.properties.isCapsLock) ? content.toUpperCase() : content;
+    const textArea = this.elements.textarea;
+    textArea.setRangeText(value, textArea.selectionStart, textArea.selectionEnd);
     this.elements.textarea.focus();
-    this.elements.textarea.selectionStart = this.elements.textarea.value.length;
+    this.elements.textarea.selectionStart = this.elements.textarea.selectionEnd + 1;
   }
 
   capsLockOn() {
     this.properties.isCapsLock = !this.properties.isCapsLock;
     const keys = this.elements.keyboardContainer.querySelectorAll('.symbol');
-    /* eslint-disable no-return-assign, no-param-reassign */
-    keys.forEach((key) => (key.textContent = this.properties.isCapsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase()));
-    /* eslint-enable no-return-assign, no-param-reassign */
+    keys.forEach((key) => {
+      const el = key;
+      el.textContent = this.properties.isCapsLock
+        ? el.textContent.toUpperCase()
+        : el.textContent.toLowerCase();
+    });
+  }
+
+  shiftKeyOn() {
+    this.properties.isShiftPressed = !this.properties.isShiftPressed;
   }
 
   render() {
@@ -147,11 +156,9 @@ export default class Keyboard {
     this.elements.main.appendChild(this.elements.textarea);
     this.elements.main.appendChild(this.elements.keyboardContainer);
     this.elements.keyboardContainer.appendChild(this.createKeys());
-
-    this.elements.keys = this.elements.keyboardContainer.querySelectorAll('.key');
   }
 
   toString() {
-    return this.elements.keys;
+    return this.properties;
   }
 }
